47e10400151d5c5f2743fc515b969cb6577ac217
Bogdan <bogdan.davis10@gmail.com>
2026-08-12 02:24:11 +0300

    Moved Item.java into a package

--- src/itemUtils/Item.java
+++ src/itemUtils/Item.java
@@ -1,12 +1,14 @@
+package itemUtils;
+
 /*
     Since this is eventually going to be used for Minecraft Forge purposes,
     I will try to make classes Minecraft oriented.
  */
 public class Item {
-    String name; // I decided Item having only name for now is enough.
+    String name; // I decided itemUtils.Item having only name for now is enough.
 
     public Item(String name) {
-        if(name == null) throw new IllegalArgumentException("Item must have a name!");
+        if(name == null) throw new IllegalArgumentException("itemUtils.Item must have a name!");
         this.name = name;
     }
 }
