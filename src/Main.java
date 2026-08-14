76f2ea1d561991f31c6ce9592c2f2d9936295ec1
Bogdan <bogdan.davis10@gmail.com>
2026-08-14 14:02:31 +0300

    Update Item Consturctor to use ItemCatergory

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import itemUtils.Item;
+import itemUtils.ItemCategory;
 import itemUtils.ItemStack;
 
 public class Main {
     public static void main(String[] args) {
     // Testing my constructors for invariants.
-    Item apple = new Item("Apple");
-    ItemStack appleStack = new ItemStack(null, 0);
+    Item apple = new Item("Apple", ItemCategory.FOOD);
+    ItemStack appleStack = new ItemStack(apple, 0);//Changed this back from null since im done testing.
 
     }
 }
--- src/itemUtils/Item.java
+++ src/itemUtils/Item.java
@@ -6,10 +6,12 @@ package itemUtils;
  */
 public class Item {
     String name; // I decided itemUtils.Item having only name for now is enough.
+    ItemCategory itemCategory;
 
-    public Item(String name) {
+    public Item(String name, ItemCategory itemCategory) {
         if(name == null) throw new IllegalArgumentException("itemUtils.Item must have a name!");
         this.name = name;
+        this.itemCategory = itemCategory;
     }
 
     //Getters
