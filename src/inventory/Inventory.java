9d1b0895932693c12d621018485089c73bb8b947
Bogdan <bogdan.davis10@gmail.com>
2026-08-16 08:56:16 +0300

    Add method for item addition

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -12,6 +12,27 @@ public class Inventory {
     public Inventory(){
     }
 
+    //First I need to check the conditions
+    public boolean addItem(Item item, int quantity){
+        if(item == null || quantity <= 0)
+            return false;
+        //Loop through inventory
+        for(ItemStack stack : inventory){
+            //If this item exists in a stack we only modify the quantity
+            if(stack.hasItem(item)){
+                stack.setQuantity(stack.getQuantity() + quantity);
+                return true;
+            }
+        }
+
+
+        //Might handle inventory size later
+
+
+        //We create a new stack if the item doesnt exist yet
+        inventory.add(new ItemStack(item, quantity));
+        return true;
+    }
     public boolean getContent(){
         for(ItemStack stack : inventory){
             System.out.println(stack.getItem().getName() + "x" + stack.getQuantity());
