3b3407cbe4e96cba8904ee5765f69090a1153569
Bogdan <bogdan.davis10@gmail.com>
2026-08-16 08:16:51 +0300

    Add ArrayList to Inventory

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -3,7 +3,10 @@ package inventory;
 import itemUtils.Item;
 import itemUtils.ItemStack;
 
+import java.util.ArrayList;
+
 public class Inventory {
+    ArrayList<ItemStack> inventory = new ArrayList<>();
     ItemStack itemStack;
 
     public Inventory(){
