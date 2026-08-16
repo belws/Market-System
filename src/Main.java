d0abbaf3432c3ffaafd789d13a509b845baf82a5
Bogdan <bogdan.davis10@gmail.com>
2026-08-16 08:59:06 +0300

    Updated Main cleared

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,6 @@ import itemUtils.ItemStack;
 
 public class Main {
     public static void main(String[] args) {
-        // Testing my constructors for invariants.
-        Item apple = new Item ("Apple", ItemCategory.FOOD);
-        Inventory inventory = new Inventory();
-        inventory.addItem(apple, 1);
-        inventory.getContent();
-        inventory.addItem(apple, 1);
-        inventory.getContent();
-        inventory.addItem(apple, 0);
-        inventory.getContent();
-        inventory.addItem(apple, -1);
-        inventory.getContent();
-        inventory.addItem(new Item("Sword", ItemCategory.EQUIPMENT), 1);
-        inventory.getContent();
 
     }
 }
