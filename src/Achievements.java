d19e4ea7d23361883851168e7eac25ce6e0c5c18
Bogdan <bogdan.davis10@gmail.com>
2026-08-20 20:46:14 +0300

    Refactor Enum to have ID and proper consturctor + getters

--- src/Achievements.java
+++ src/Achievements.java
@@ -1,9 +1,19 @@
 public enum Achievements {
-    GETTING_STARTED("Your collecting journey has began!"),
-    WAY_TO_GO("You collected 10 Apples!"),
-    MASTERFUL_COLLECTION("100 Apples is an impressive feat");
+    GETTING_STARTED(0,"Your collecting journey has began!"),
+    WAY_TO_GO(1, "You collected 10 Apples!"),
+    MASTERFUL_COLLECTION(2, "100 Apples is an impressive feat");
+    private final int id;
+    private final String s;
 
-    Achievements(String s) {
+    Achievements(int id, String s) {
+        this.id = id;
+        this.s = s;
 
+    }
+    public int getId(){
+        return id;
+    }
+    public String getMessage(){
+        return s;
     }
 }
