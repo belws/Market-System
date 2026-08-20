017ae194ec8042912587678288cf6b6da14d0aac
Bogdan <bogdan.davis10@gmail.com>
2026-08-20 19:55:09 +0300

    Made a temporary enum for Achievements

---
+++ src/Achievements.java
@@ -0,0 +1,9 @@
+public enum Achievements {
+    GETTING_STARTED("Your collecting journey has began!"),
+    WAY_TO_GO("You collected 10 Apples!"),
+    MASTERFUL_COLLECTION("100 Apples is an impressive feat");
+
+    Achievements(String s) {
+
+    }
+}
