3df110589cd1b31eec48a6a07a19326bcb1f5d28
Bogdan <bogdan.davis10@gmail.com>
2026-08-20 10:16:27 +0300

    Getting started with HashMap and first exercise

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,30 @@ import itemUtils.Item;
 import itemUtils.ItemCategory;
 import itemUtils.ItemStack;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Here we go again :)
+        Today's lesson is HashMap and from what I gathered so far this represents the relationship between a key and a value.
+         */
+        HashMap<String, Integer> items = new HashMap<>();
+
+        //1 Put "Apple" → 50
+        items.put("Apple", 50);
+        //2 Put "Diamond" → 12
+        items.put("Diamond", 12);
+        //3 Put "Iron" → 64
+        items.put("Iron", 64);
+        //4 Print the value associated with "Diamond"
+        System.out.println(items.get("Diamond"));
+        //5 Change "Diamond" to 20
+        items.put("Diamond", 20);
+        //6 Print the value associated with "Diamond" again
+        System.out.println(items.get("Diamond"));
+        //7 Print the size of the map
+        System.out.println(items.size());
     }
 }
